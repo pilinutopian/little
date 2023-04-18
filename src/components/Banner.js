c25cb2a5fbcdac22df9beed055332b77d3e00675
Pilar <pilar.casanueva@gmail.com>
2023-04-18 11:39:23 +0200

    Components updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/pili.jpg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 //import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -9,10 +9,10 @@ export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(200 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Programmer"];
-  const period = 2000;
+  const toRotate = [ "Web Developer", "Programmer", "Artist"];
+  const period = 1000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -50,13 +50,13 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Pilar,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='["Web Developer", "Programmer"]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Pilar Casanueva,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='["Web Developer", "Programmer", "Artist"]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
@@ -66,7 +66,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  <img className='pili' src={headerImg} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
